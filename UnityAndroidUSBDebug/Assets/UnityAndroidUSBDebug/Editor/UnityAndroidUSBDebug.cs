5dc34cfd7d16eb42aaf3e4a57884009ce95d6a26
kkkon <kiyofumi.kondoh+github@gmail.com>
2018-01-30 07:55:57 +0900

    add device select

--- UnityAndroidUSBDebug/Assets/UnityAndroidUSBDebug/Editor/UnityAndroidUSBDebug.cs
+++ UnityAndroidUSBDebug/Assets/UnityAndroidUSBDebug/Editor/UnityAndroidUSBDebug.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 
 #if UNITY_EDITOR
@@ -15,7 +16,9 @@ public class UnityAndroidUSBDebug
     private static string KEY_ANDROID_SDK = "AndroidSdkRoot";
     private static string KEY_ANDROID_NDK = "AndroidNdkRoot";
 
-    static void invoke( string name, string[] args, bool waitFinish = true )
+    private static string mDeviceSerial = string.Empty;
+
+    static string invoke( string name, string[] args, bool waitFinish = true )
     {
         var info = new System.Diagnostics.ProcessStartInfo();
 
@@ -23,7 +26,11 @@ public class UnityAndroidUSBDebug
         {
             info.EnvironmentVariables["ANDROID_SDK_ROOT"] = EditorPrefs.GetString(KEY_ANDROID_SDK);
         }
-        //info.EnvironmentVariables["ANDROID_SERIAL"] = "xxxx"; // for adb
+        if ( 0 < mDeviceSerial.Length )
+        {
+            //Debug.Log( "mDeviceSerial.Length=" + mDeviceSerial.Length );
+            info.EnvironmentVariables["ANDROID_SERIAL"] = mDeviceSerial; // for adb
+        }
         info.UseShellExecute = false;
 
         info.FileName = name;
@@ -77,6 +84,8 @@ public class UnityAndroidUSBDebug
                     }
                 }
 
+                //Debug.Log( "STDOUT: " + sbOut.ToString() );
+                //Debug.Log( "STDERR: " + sbErr.ToString() );
                 if ( 0 != process.ExitCode )
                 {
                     throw new System.Exception("ExitCode=" + process.ExitCode + System.Environment.NewLine
@@ -95,6 +104,10 @@ public class UnityAndroidUSBDebug
             try { process.Close(); } catch ( System.Exception ) { }
             try { process.Dispose(); } catch ( System.Exception ) { }
         }
+
+        string output = null;
+        output = sbOut.ToString();
+        return output;
     }
 
     //[MenuItem("UniAndUSBDebug/Test")]
@@ -150,7 +163,172 @@ public class UnityAndroidUSBDebug
         });
     }
 
+
+
+
+    public class DeviceInfo
+    {
+        public string mSerial;
+        public string mDesc;
+    }
+
+    public static List<DeviceInfo> getDevices()
+    {
+        string output = null;
+
+        output = invoke( EditorPrefs.GetString(KEY_ANDROID_SDK) + "/platform-tools/adb", new string[] {
+            "devices"
+            , "-l"
+        });
+
+        if ( null == output )
+        {
+            return null;
+        }
+
+        //Debug.Log( output );
+        string[] lines = output.Split( '\n' );
+        if ( null == lines )
+        {
+            return null;
+        }
+
+        List<DeviceInfo> listDevInfo = new List<DeviceInfo>();
+        {
+            var devInfo = new DeviceInfo();
+            devInfo.mSerial = "     "; //string.Empty;
+            devInfo.mDesc = "(no select)";
+            listDevInfo.Add( devInfo );
+        }
+        foreach ( var line in lines )
+        {
+            if ( line.StartsWith( "List" ) )
+            {
+                continue;
+            }
+            if ( line.StartsWith( "adb server" ) )
+            {
+                continue;
+            }
+            if ( line.StartsWith( "* daemon" ) )
+            {
+                continue;
+            }
+
+            string[] tokens = line.Split( new char[] { ' ', '\t' }, 2 );
+            if ( null == tokens )
+            {
+                continue;
+            }
+            if ( tokens.Length < 2 )
+            {
+                continue;
+            }
+            //Debug.Log( tokens[0] + "," + tokens[1] );
+            tokens[0] = tokens[0].Trim();
+            tokens[1] = tokens[1].Trim();
+
+            var devInfo = new DeviceInfo();
+            devInfo.mSerial = tokens[0];
+            devInfo.mDesc = tokens[1];
+            listDevInfo.Add( devInfo );
+        }
+
+        return listDevInfo;
+    }
+
+    public static string getDeviceSerial()
+    {
+        return mDeviceSerial;
+    }
+
+    public static void setDeviceSerial( string serial )
+    {
+        if ( null == serial )
+        {
+            serial = string.Empty;
+        }
+        mDeviceSerial = serial.Trim();
+    }
+
+    [MenuItem("UniAndUSBDebug/SelectDevice", false, 1000)]
+    static void SelectDevice()
+    {
+        UniAndDeviceSelect.Init();
+    }
 }
+
+public class UniAndDeviceSelect: EditorWindow
+{
+    private static List<UnityAndroidUSBDebug.DeviceInfo> mDeviceInfo;
+    private static int mIndex = 0;
+    private static GUIContent[] mContent;
+
+    public static void selectSerial( string serial )
+    {
+        var count = mDeviceInfo.Count;
+        mIndex = 0;
+        for ( int index = 0; index < count; ++index )
+        {
+            if ( serial.Equals( mContent[index].text ) )
+            {
+                mIndex = index;
+            }
+        }
+    }
+
+    public static void Init()
+    {
+        var window = GetWindow( typeof(UniAndDeviceSelect) );
+        ListRefresh();
+        window.Show();
+    }
+
+    static void ListRefresh()
+    {
+        mDeviceInfo = UnityAndroidUSBDebug.getDevices();
+
+        var serial = string.Empty;
+        if ( null == mContent )
+        {
+            serial = UnityAndroidUSBDebug.getDeviceSerial();
+        }
+        else
+        {
+            serial = mContent[mIndex].text;
+        }
+
+        {
+            var count = mDeviceInfo.Count;
+            mContent = new GUIContent[count];
+            for ( int index = 0; index < count; ++index )
+            {
+                var content = new GUIContent();
+                content.text    = mDeviceInfo[index].mSerial;
+                content.tooltip = mDeviceInfo[index].mDesc;
+                mContent[index] = content;
+            }
+        }
+
+        selectSerial( serial );
+    }
+
+    void OnGUI()
+    {
+        if ( GUILayout.Button("Refresh") )
+        {
+            ListRefresh();
+        }
+
+        var index = EditorGUILayout.Popup( mIndex, mContent );
+        if ( index != mIndex )
+        {
+            UnityAndroidUSBDebug.setDeviceSerial( mContent[index].text );
+        }
+        mIndex = index;
+    }
+}
+
 
 } // namespace UniAndUSBDebug
 
