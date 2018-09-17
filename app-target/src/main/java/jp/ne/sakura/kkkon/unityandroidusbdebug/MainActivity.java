2ffefa4c83e3c2939d6059abca567e7ddc471720
kkkon <kiyofumi.kondoh+github@gmail.com>
2018-09-17 11:28:41 +0900

    enhancement. use flag and display error status

--- app-target/src/main/java/jp/ne/sakura/kkkon/unityandroidusbdebug/MainActivity.java
+++ app-target/src/main/java/jp/ne/sakura/kkkon/unityandroidusbdebug/MainActivity.java
@@ -4,14 +4,22 @@ package jp.ne.sakura.kkkon.unityandroidusbdebug;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import jp.ne.sakura.kkkon.lib.DebugLog;
 
 public class MainActivity extends Activity {
 
     private static final String TAG = "MainActivity";
+
+    LinearLayout    mLayout = null;
+    TextView        mTextView = null;
+    Button          mButtonStart = null;
+    Button          mButtonStop = null;
+    ThreadMonitor   mThreadMonitor = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -24,9 +32,19 @@ public class MainActivity extends Activity {
         layout.setLayoutParams( new LinearLayout.LayoutParams( LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT ));
 
         {
+            TextView text = new TextView( this );
+            text.setTextSize( 24 );
+            text.setText( "" );
+
+            mTextView = text;
+            layout.addView( text );
+        }
+
+        {
             Button button = new Button(this);
             button.setText("listen start");
 
+            mButtonStart = button;
             layout.addView(button);
 
             button.setOnClickListener(new View.OnClickListener()
@@ -37,6 +55,10 @@ public class MainActivity extends Activity {
                     DebugLog.d(TAG, "threadStart");
                     MulticastReceiver.threadStart();
                     MulticastTunnel.threadStart();
+                    threadMonitorStop();
+                    threadMonitorStart();
+                    mTextView.setText("");
+                    mButtonStop.setEnabled(true);
                 }
             });
 
@@ -45,7 +67,9 @@ public class MainActivity extends Activity {
         {
             Button button = new Button(this);
             button.setText("listen stop");
+            button.setEnabled(false);
 
+            mButtonStop = button;
             layout.addView(button);
 
             button.setOnClickListener(new View.OnClickListener()
@@ -56,11 +80,113 @@ public class MainActivity extends Activity {
                     DebugLog.d(TAG, "threadStop");
                     MulticastTunnel.threadStop();
                     MulticastReceiver.threadStop();
+                    threadMonitorStop();
+                    mButtonStart.setEnabled(true);
+                    mButtonStop.setEnabled(false);
                 }
             });
 
         }
 
+        mLayout = layout;
         setContentView( layout );
     }
+
+    @Override
+    protected void onDestroy()
+    {
+        MulticastTunnel.threadStop();
+        MulticastReceiver.threadStop();
+
+        if ( null == mThreadMonitor )
+        {
+            mThreadMonitor.interrupt();
+            try
+            {
+                mThreadMonitor.join();
+            }
+            catch ( InterruptedException e )
+            {
+
+            }
+            mThreadMonitor = null;
+        }
+
+        if ( null != mButtonStop )
+        {
+            mButtonStop.setOnClickListener( null );
+            mButtonStop = null;
+        }
+        if ( null != mButtonStart )
+        {
+            mButtonStart.setOnClickListener( null );
+            mButtonStart = null;
+        }
+
+        {
+            final ViewGroup viewParent = (ViewGroup) mLayout.getParent();
+            viewParent.removeAllViews();
+        }
+
+        super.onDestroy();
+    }
+
+    protected void threadMonitorStop()
+    {
+        if ( null != mThreadMonitor )
+        {
+            mThreadMonitor.interrupt();
+            try
+            {
+                mThreadMonitor.join();
+            }
+            catch ( InterruptedException e )
+            {
+
+            }
+            mThreadMonitor = null;
+        }
+    }
+
+    protected void threadMonitorStart()
+    {
+        if ( null == mThreadMonitor )
+        {
+            mThreadMonitor = new ThreadMonitor();
+            mThreadMonitor.start();
+        }
+    }
+
+    private class ThreadMonitor extends Thread
+    {
+
+        @Override
+        public void run()
+        {
+            for ( ; ; )
+            {
+                try
+                {
+                    Thread.sleep(30 * 1000);
+                    if ( MulticastReceiver.isThreadRecvSocketError() )
+                    {
+                        MainActivity.this.runOnUiThread( new Runnable() {
+                                @Override
+                                public void run()
+                                {
+                                    MainActivity.this.mTextView.setText("Occured Socket Error!!");
+                                }
+                        });
+
+                        break;
+                    }
+                }
+                catch ( InterruptedException e )
+                {
+                    break;
+                }
+            }
+        }
+    }
+
 }
