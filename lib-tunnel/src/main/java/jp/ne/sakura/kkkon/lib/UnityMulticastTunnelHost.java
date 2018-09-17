ca092af8ed64ff8ec99881e965e475f64fb4360c
kkkon <kiyofumi.kondoh+github@gmail.com>
2018-09-17 17:44:05 +0900

    bug fix. not always success, socket connect

--- lib-tunnel/src/main/java/jp/ne/sakura/kkkon/lib/UnityMulticastTunnelHost.java
+++ lib-tunnel/src/main/java/jp/ne/sakura/kkkon/lib/UnityMulticastTunnelHost.java
@@ -70,6 +70,11 @@ public class UnityMulticastTunnelHost
             init();
         }
 
+        if ( null == mSocket )
+        {
+            return null;
+        }
+
         InputStream in = null;
         UnityPlayerInfo info = null;
 
